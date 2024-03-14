037e18cb0d4f997e89b4686c19fe77af39952ff6
Muhammad Abdullah Mushtaq <154248651+abdu19ah@users.noreply.github.com>
2024-03-14 18:13:21 +0500

    Time: 0 ms (100.00%) | Memory: 41.9 MB (75.68%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -3,15 +3,13 @@ class Solution {
         
         int itr = nums1.length - 1;
         m--; n--;
-        
-        while(m >= 0 && n >= 0) {
-            if(nums1[m] > nums2[n])
+
+        while(n >= 0) {
+            if(m >= 0 && nums1[m] > nums2[n])
                 nums1[itr--] = nums1[m--];
             else
                 nums1[itr--] = nums2[n--];
         }
-        for( ; n >= 0 ; ) 
-            nums1[itr--] = nums2[n--];
          
     }
 }
